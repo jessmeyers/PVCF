f7fb600456c9da4a8f29947696868b8fd3dd3c83
jessmeyers <53497726+jessmeyers@users.noreply.github.com>
2019-11-19 14:20:47 -0500

    Add files via upload

---
+++ PVFC.js
@@ -0,0 +1,39 @@
+<head>
+
+  <title>Pottersville Volunteer Fire Company</title>
+  <link rel="stylesheet" href="PVFC.css">
+</head>
+
+<body>
+<br>
+
+<h1>
+  <center id="front"><b>Pottersville Volunteer Fire Company</b></center>
+</h1>
+<br>
+
+<center>
+
+  <a href="home.html" class="navtabs"> <b>Home </b> </a>
+  <a href="calendar.html" class="navtabs"> <b>Calendar </b> </a>
+  <a href="members.html" class="navtabs"> <b>Members </b> </a>
+  <a href="history.html" class="navtabs"> <b>History </b> </a>
+  <a href="volunteer.html" class="navtabs"> <b>Volunteer </b> </a>
+  <a href="donate.html" class="navtabs"> <b>Donate </b> </a>
+  <a href="safety.html" class="navtabs"> <b>Home Safety </b> </a>
+  <a href="antiqueshow.html" class="navtabs"> <b>Antique Show </b> </a>
+<br>
+<br>
+<img id="pic" src="PVFC.jpg">
+<br>
+<br>
+  <div><id = "welcome">WELCOME</div>
+        <br><div id="hometext">Welcome to the Pottersville Volunteer Fire Company website. We are a Bedminster Township Fire Company serving Bedminster Township, Somerset County, and Tewksbury Township, Hunterdon County since 1954. We are a 100% volunteer fire company located in the Pottersville area of Bedminster Township, Somerset County, New Jersey. Our diverse membership performs this service of his or her own free will without any compensation.
+
+
+Mission
+Our primary mission is to protect to the best of our ability the lives and property of our neighbors and the families of the Pottersville area. Our secondary mission is to support to the best of our ability our mutual aid companies when requested.
+Membership inquiries welcomed. Click on the Volunteer link for more information. Please feel free to browse our website. Comments welcomed.</div>
+
+</body>
+</html>
